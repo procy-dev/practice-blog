bbfc3bba0a56f5eee63ad4637abd58e7deb42874
Chris Winkler <procy.dev@gmail.com>
2024-03-21 09:44:07 -0600

    finalize friend stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :friends
+  resources :friends # Creates all CRUD routes for friends model
   root 'home#index'
-
   get 'home/about'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
